9feacf388b65a2a55371f1b998848c39462d8855
Sam Odele <odelesamson@gmail.com>
2024-07-25 14:52:25 +0100

    Update App config file

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Google 2FA Auth'),
+    'name' => env('APP_NAME', 'Google 2FA Authentication'),
 
     /*
     |--------------------------------------------------------------------------
