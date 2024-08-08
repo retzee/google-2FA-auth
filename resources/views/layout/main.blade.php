12345654e01fc6ab23c0af8809695b785b099593
Sam Odele <odelesamson@gmail.com>
2024-08-08 23:31:42 +0100

    Update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Google 2FA Authentication</title>
+    <title>Google 2FA Authentication App</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/fontawesome-all.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/main_style.css') }}">
--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -3,59 +3,13 @@
 @section('content')
 
 <div class="header_text_container">
-    <h3>Teacher Dashboard</h3>
+    <h3>Member Dashboard</h3>
 </div>
 
 
     <div class="form-holder">
         <div class="form-content">
-            <div class="form-items">
-                
-                <div class="row">
-                    <div class="col-md-6 col-sm-12">
-                        <a href="{{url('/take-attendance')}}">
-                            <div class="widget_box widget_yellow rounded_edge_box">
-                                <h4>Take My Attendance</h4>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-6 col-sm-12">
-                        <a href="{{url('/attendance-log')}}">
-                            <div class="widget_box widget_red rounded_edge_box">
-                                <h4>My Attendance Log</h4>
-                            </div>
-                        </a>
-                    </div>
-
-
-                    <div class="col-md-6 col-sm-12">
-                        <a href="{{url('/take-student-attendance')}}">
-                            <div class="widget_box widget_blue rounded_edge_box">
-                                <h4>Class Attendance</h4>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-6 col-sm-12">
-                        <a href="{{url('/2fa')}}">
-                            <div class="widget_box widget_green rounded_edge_box">
-                                <h4>2FA Setting</h4>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-6 col-sm-12">
-                        <a href="{{url('/take-subject-attendance')}}">
-                            <div class="widget_box widget_purple rounded_edge_box">
-                                <h4>Subject Attendance</h4>
-                            </div>
-                        </a>
-                    </div>
-
-                </div>
-
-            </div>
+            <div class="form-items"></div>
         </div>
     </div>
        
