2af333ccc7a9040c1fab0f45c81cebc8e02b080c
Sam Odele <odelesamson@gmail.com>
2024-08-08 23:29:39 +0100

    Update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,10 +17,10 @@
 
                     <div class="row">
                         <div class="col-12 col-sm-6">
-                            <input type="text" name="username" class="form-control" placeholder="Username">
+                            <input type="text" name="username" class="form-control" placeholder="Type your Username">
                         </div>
                         <div class="col-12 col-sm-6">
-                            <input type="password" name="password" class="form-control" placeholder="Password">
+                            <input type="password" name="password" class="form-control" placeholder="Type your Password">
                         </div>
                     </div>
 
