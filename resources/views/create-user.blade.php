c58455e75b0208b250870794409aec1aa53c7fa3
Sam Odele <odelesamson@gmail.com>
2024-08-08 23:30:45 +0100

    Update

--- resources/views/create-user.blade.php
+++ resources/views/create-user.blade.php
@@ -16,19 +16,19 @@
                      {{csrf_field()}}
                     <div class="row">
                         <div class="col-12 col-sm-6">
-                            <input type="text" name="username" class="form-control" placeholder="Email">
+                            <input type="text" name="username" class="form-control" placeholder="Type your Email">
                         </div>
                         <div class="col-12 col-sm-6">
-                            <input type="password" name="password" class="form-control" placeholder="Password">
+                            <input type="password" name="password" class="form-control" placeholder="Type your Password">
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-12 col-sm-6">
-                            <input type="text" name="first_name" class="form-control" placeholder="First Name">
+                            <input type="text" name="first_name" class="form-control" placeholder="Type your First Name">
                         </div>
                         <div class="col-12 col-sm-6">
-                            <input type="text" name="surname" class="form-control" placeholder="Surname">
+                            <input type="text" name="surname" class="form-control" placeholder="Type your Surname">
                         </div>
                     </div>
 
